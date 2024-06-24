fe67750f17dc20cfa31333b4552f244a8ecd1967
FinochioAdrian <eidrienhez33@gmail.com>
2024-06-24 00:41:10 -0300

    Fix faker library

--- src/utils/Mocks.js
+++ src/utils/Mocks.js
@@ -1,4 +1,4 @@
-import {faker} from '@faker-js/Faker'
+import { faker } from '@faker-js/faker';
 faker.location= "es"
     export const generateListProducts = async ()  => {
         let numOfProducts = 100
